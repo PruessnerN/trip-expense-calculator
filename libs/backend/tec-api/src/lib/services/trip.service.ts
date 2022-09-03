1626a599d2625d8152d60bd4addba5ae319f859f
Npruessner <npruessner@protonmail.com>
2022-09-03 15:29:53 -0500

    added slug generator

--- libs/backend/tec-api/src/lib/services/trip.service.ts
+++ libs/backend/tec-api/src/lib/services/trip.service.ts
@@ -7,10 +7,16 @@ import { Repository } from 'typeorm';
 import { Trip } from '../entities/trip.entity';
 
 export class TripService {
+  private readonly SLUG_NUM_MIN = 10000;
+  private readonly SLUG_NUM_MAX = 99999;
+
   constructor(@InjectRepository(Trip) private repository: Repository<Trip>) {}
 
   async create(createTripDto: CreateTripDto): Promise<Trip> {
-    return await this.repository.create(createTripDto);
+    return await this.repository.save({
+      ...createTripDto,
+      slug: this.generateSlug(createTripDto.name),
+    });
   }
 
   async findAll(): Promise<Trip[]> {
@@ -33,5 +39,17 @@ export class TripService {
     const result = await this.repository.softDelete(id);
 
     return result.affected === 1;
+  }
+
+  private generateSlug(name: string): string {
+    const num =
+      Math.floor(Math.random() * (this.SLUG_NUM_MAX - this.SLUG_NUM_MIN + 1)) + this.SLUG_NUM_MIN;
+
+    return (
+      name
+        .toLocaleLowerCase()
+        .replace(' ', '-')
+        .replace(/[^a-zA-Z0-9-]/g, '') + `-${num}`
+    );
   }
 }
