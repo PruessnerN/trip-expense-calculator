1d575fe12818b5ce945adafe3d9a486a89d9a683
Npruessner <npruessner@protonmail.com>
2022-09-04 10:59:07 -0500

    styled modal and created second modal

--- libs/frontend/feature-trip/src/lib/pages/trip/trip.component.ts
+++ libs/frontend/feature-trip/src/lib/pages/trip/trip.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TripDto } from '@trip-expense-calculator/api-interfaces';
 import { TripService } from '@trip-expense-calculator/frontend/data-access-trip';
@@ -12,6 +13,18 @@ import { Observable, catchError, switchMap } from 'rxjs';
 })
 export class TripComponent {
   trip$: Observable<TripDto>;
+
+  expenseForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    value: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^\$?[0-9]+(\.[0-9][0-9])?$/),
+    ]),
+  });
+
+  memberForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+  });
 
   constructor(
     private modalService: ModalService,
@@ -34,6 +47,22 @@ export class TripComponent {
 
   onAddExpenseClick(): void {
     this.modalService.open('expense-modal');
+  }
+
+  onSaveExpenseClick(): void {
+    if (this.expenseForm.valid) {
+      console.log(this.expenseForm.value);
+      this.closeModal('expense-modal');
+      this.expenseForm.reset();
+    }
+  }
+
+  onSaveMemberClick(): void {
+    if (this.memberForm.valid) {
+      console.log(this.memberForm.value);
+      this.closeModal('member-modal');
+      this.memberForm.reset();
+    }
   }
 
   closeModal(id: string) {
