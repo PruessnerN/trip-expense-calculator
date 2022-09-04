4f042d9b814d78b4df63f74f31d36129d8c65dde
Npruessner <npruessner@protonmail.com>
2022-09-04 17:12:18 -0500

    formatting changes

--- libs/frontend/feature-trip/src/lib/trip.module.ts
+++ libs/frontend/feature-trip/src/lib/trip.module.ts
@@ -5,14 +5,20 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '@trip-expense-calculator/frontend/util-shared';
 
+import { SettleUpComponent } from './pages/trip/components/settle-up/settle-up.component';
 import { TripComponent } from './pages/trip/trip.component';
 import { CreateTripComponent } from './pages/trips/components/create-trip/create-trip.component';
 import { TripSelectionComponent } from './pages/trips/components/trip-selection/trip-selection.component';
 import { TripsComponent } from './pages/trips/trips.component';
-import { SettleUpComponent } from './pages/trip/components/settle-up/settle-up.component';
 
 @NgModule({
-  declarations: [TripsComponent, CreateTripComponent, TripSelectionComponent, TripComponent, SettleUpComponent],
+  declarations: [
+    TripsComponent,
+    CreateTripComponent,
+    TripSelectionComponent,
+    TripComponent,
+    SettleUpComponent,
+  ],
   imports: [CommonModule, FlexLayoutModule, ReactiveFormsModule, RouterModule, SharedModule],
   exports: [TripsComponent],
 })
